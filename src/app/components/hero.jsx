191c9281a31d705df8485e1432be0d8bc83af1d2
rikkejuliane <rikand01864@stud.noroff.no>
2025-07-21 17:53:41 +0200

    think ive finished the hero section, was having a pain in the ass with it, think mobile first is so difficult

---
+++ src/app/components/hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="hero-image">
+        <img
+          src="hero-image.jpg"
+          alt="Hero Image"
+        />
+      </div>
+    </section>
+  );
+}

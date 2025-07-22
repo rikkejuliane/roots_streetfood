b59fb7d58166c5def77f8cf9fef8b93336698ea5
rikkejuliane <rikand01864@stud.noroff.no>
2025-07-22 13:20:35 +0200

    added the menus to the menu section

---
+++ src/app/components/menu.jsx
@@ -0,0 +1,9 @@
+export default function Menu(){
+  return(
+    <section>
+    <h1 className="mt-4">TAKE A LOOK AT OUR MENU</h1>
+    <img src="menu-part1.jpg" alt="Menu for roots"/>
+    <img src="menu-part2.jpg" alt="Menu for roots"/>
+    </section>
+  )
+}

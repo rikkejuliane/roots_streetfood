c383f313e35cf1adff78ce717d778a58868a6f0c
rikkejuliane <rikand01864@stud.noroff.no>
2025-08-03 13:15:29 +0200

    updated metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,47 @@ import Footer from "./components/footer";
 import { LanguageProvider } from "./utils/languageContext";
 
 export const metadata: Metadata = {
-  title: "Roots Street Food",
-  description: "Delicious street food served fresh in [Your Location]",
+  title: "Roots Fast Food | Fast Food u Posedarju, Zadru",
+  description:
+    "Ukusna ulična hrana svježe pripremljena u Posedarju, Zadru. Uživajte u autentičnim okusima i ugodnoj atmosferi.",
+  keywords: [
+    "Roots Fast Food",
+    "hrana u Posedarju",
+    "hrana u Zadru",
+    "hrvatska ulična hrana",
+    "brza hrana",
+    "lokalna hrana",
+    "restoran",
+    "za van",
+    "Roots fast Food",
+    "Posedarje food",
+    "Zadar food",
+    "Croatian street food",
+    "fast food",
+    "local food",
+    "restaurant",
+    "takeaway",
+  ],
+  authors: [
+    { name: "Roots Street Food" },
+    { name: "Rikke Juliane (Website by)" },
+  ],
+  openGraph: {
+    title: "Roots Street Food - Posedarje, Zadar",
+    description:
+      "Ukusna ulična hrana svježe pripremljena u Posedarju, Zadru, Hrvatska. Uživajte u autentičnim okusima i ugodnoj atmosferi.",
+    url: "https://rootsstreetfood.example.com", // Replace with your actual URL
+    siteName: "Roots Street Food",
+    locale: "hr_HR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="hr">
       <body className="antialiased">
         <LanguageProvider>
           <Header />

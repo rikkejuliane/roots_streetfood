fc2a514130bd5495d223bf1bfeb49caf9ec2c8ab
Rikke Juliane <152181168+rikkejuliane@users.noreply.github.com>
2026-06-23 11:27:43 +0200

    updated opening hours

--- src/app/components/openingHours.jsx
+++ src/app/components/openingHours.jsx
@@ -8,22 +8,22 @@ export default function OpeningHours() {
   return (
     <section className="mt-4">
       <h1>{translations[lang].openingHours}</h1>
-      {/* <div className="flex flex-col justify-center items-center">
+       <div className="flex flex-col justify-center items-center">
         <p className="text-[25px] font-semibold font-montserrat">
           16
           <span className="align-super text-xs tracking-wide px-[2px]">
             00
           </span>{" "}
-          - 23
+          - 22
           <span className="align-super text-xs tracking-wide px-[2px]">00</span>
         </p>
         <p className="font-montserrat text-[12px] font-bold">
           {translations[lang].monSun}
         </p>
-      </div> */}
-      <div>
+      </div> 
+      {/* <div>
         <p className="text-center font-montserrat text-[16px] font-bold">{translations[lang].winterTime}</p>
-      </div>
+      </div> */}
     </section>
   );
 }
